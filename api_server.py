1983c080b1d680d2eaf7837244dc4cea0ac6b112
jongjin pak <belovian@gmail.com>
2024-09-20 00:46:33 +0900

    feat: split jobquq.

--- api_server.py
+++ api_server.py
@@ -1,97 +1,25 @@
 import asyncio
-from contextlib import asynccontextmanager
 import logging
 import queue
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from typing import List, Optional
-import os
 import sys
 
 from deepl_bot import DeepLBot
+from job_queue import JobQueue
 
 app = FastAPI()
 MAX_JOB=2
 
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.basicConfig(
     stream=sys.stdout, 
     level=logging.INFO, 
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - %(process)d'
 )
-
-#@asynccontextmanager
-#async def lifespan(app):
-#    async with app:
-#        logging.info("starting~")
-
-import asyncio
-import threading
-from queue import Queue
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-class JobQueue:
-    closing:bool=False
-    def __init__(self, num_workers):
-        self.queue:Queue = Queue(maxsize=num_workers)
-        self.num_workers = num_workers
-        self.workers = []
-        self.bots:List[DeepLBot] = []
-
-    def check(self)->bool:
-        logging.info(f"check {self.queue.qsize()}/{self.queue.maxsize}")
-        if self.queue.full():
-            logging.info("check full")
-            raise queue.Full
-
-    def start(self):
-        for index in range(self.num_workers):
-            worker = threading.Thread(target=self.worker, name=f'job_{index}', daemon=True)
-            worker.start()
-            self.workers.append(worker)
-
-    def worker(self):
-        bot:DeepLBot = DeepLBot(name=threading.current_thread().name)
-        self.bots.append(bot)
-        while True:
-            job=None
-            future=None
-            try:
-                job, future = self.queue.get(timeout=0.5)
-            except queue.Empty:
-                continue
-            finally:
-                if self.closing: break
-
-            try:
-                result = asyncio.run(job())
-                logging.info('go translate')
-                translated = bot.translate(result, "ko")
-                logging.info(f'translated={str(translated)}')
-                result = JSONResponse(content={"message": f"Job completed {translated} successfully"})
-                future.set_result(result)
-            except Exception as e:
-                future.set_exception(e)
-            finally:
-                self.queue.task_done()
-
-    async def add_job(self, job):
-        future = asyncio.Future()
-        self.queue.put((job, future))
-        return await future
-
-    def stop(self):
-        logging.info("closing bots")
-        for bot in self.bots:
-            bot.close()
-
-        logging.info("closing workers")
-        self.closing = True
-        for worker in self.workers:
-            worker.join()  # 스레드가 종료될 때까지 대기
 
 job_queue = JobQueue(num_workers=MAX_JOB)
 
---
+++ job_queue.py
@@ -0,0 +1,77 @@
+from queue import Queue
+import asyncio
+import logging
+import queue
+import threading
+from typing import List
+
+from fastapi.responses import JSONResponse
+from deepl_bot import DeepLBot
+
+class JobQueue:
+  closing:bool=False
+  def __init__(self, num_workers):
+    self.queue:Queue = Queue(maxsize=num_workers)
+    self.num_workers = num_workers
+    self.workers:List[threading.Thread] = []
+    self.bots:List[DeepLBot] = []
+
+  def check(self)->bool:
+    logging.info(f"check {self.queue.qsize()}/{self.queue.maxsize}")
+    if self.queue.full():
+      logging.info("check full")
+      raise queue.Full
+
+  def start(self):
+    for index in range(self.num_workers):
+      worker = threading.Thread(target=self.worker, name=f'job_{index}', daemon=True)
+      worker.start()
+      self.workers.append(worker)
+
+  def worker(self):
+    bot:DeepLBot = DeepLBot(name=threading.current_thread().name)
+    self.bots.append(bot)
+
+    while True:
+      job=None
+      future=None
+      try:
+        logging.info(self.queue)
+        job, future = self.queue.get()
+      except queue.Empty:
+        continue
+      finally:
+        if self.closing: break
+
+      try:
+        result = asyncio.run(job())
+        logging.info('go translate')
+        translated = bot.translate(result, "ko")
+        logging.info(f'translated={str(translated)}')
+        result = JSONResponse(content={"message": f"Job completed {translated} successfully"})
+        future.set_result(result)
+      except Exception as e:
+        future.set_exception(e)
+      finally:
+        self.queue.task_done()
+
+  async def add_job(self, job):
+    future = asyncio.Future()
+    print(self.queue)
+    await self.queue.put((job, future))
+    return await future
+
+  def stop(self):
+    logging.info("closing bots")
+    for bot in self.bots:
+      try:
+        bot.close()
+      except Exception as e:
+        logging.error(e)
+
+    logging.info("closing workers")
+    self.closing = True
+    for worker in self.workers:
+      worker.join(timeout=1)  # 1초 대기 후 강제 종료
+      if worker.is_alive():
+        logging.warning(f"Worker {worker.name} did not terminate in time. Forcing termination.")
